9222e9f18d12d890db9482b3c2f65f4e016b0864
Chouette2100 <chouette.21.00@gmail.com>
2023-07-05 14:49:23 +0900

    01AA00	DBConfig,SSHConfigを一本化する。インターフェースを変更する。

--- OpenDB.go
+++ OpenDB.go
@@ -1,5 +1,5 @@
 /*!
-Copyright © 2022 chouette.21.00@gmail.com
+Copyright © 2023 chouette.21.00@gmail.com
 Released under the MIT license
 https://opensource.org/licenses/mit-license.php
 */
@@ -25,36 +25,38 @@ import (
 /*
 
 	00AA00	新規作成
+	01AA00	DBConfig,SSHConfigを一本化する。インターフェースを変更する。
+
 
 */
 
-
 type DBConfig struct {
-	UseSSH bool   `yaml:"UseSSH"`
-	Dbhost string `yaml:"DBhost"`
-	Dbport string `yaml:"DBport"`
-	Dbname string `yaml:"DBname"`
-	Dbuser string `yaml:"DBuser"`
-	Dbpw   string `yaml:"DBpw"`
-	Sracct string `yaml:"SRacct"`
-	Srpswd string `yaml:"SRpswd"`
+	UseSSH    bool   `yaml:"UseSSH"`
+	DBhost    string `yaml:"DBhost"`
+	DBport    string `yaml:"DBport"`
+	DBname    string `yaml:"DBname"`
+	DBuser    string `yaml:"DBuser"`
+	DBpswd    string `yaml:"DBpswd"`
+	SRacct    string `yaml:"SRacct"`
+	SRpswd    string `yaml:"SRpswd"`
+	SSHhost   string `yaml:"SSHhost"`
+	SSHport   int    `yaml:"SSHport"`
+	SSHuser   string `yaml:"SSHuser"`
+	SSHpswd   string `yaml:"SSHpswd"`
+	SSHprvkey string `yaml:"SSHprvkey"`
 }
-
-type SSHConfig struct {
-	Hostname   string `yaml:"Hostname"`
-	Port       int    `yaml:"Port"`
-	Username   string `yaml:"Username"`
-	Password   string `yaml:"Password"`
-	PrivateKey string `yaml:"PrivateKey"`
-}
-
-var sshconfig *SSHConfig
 
 var Dialer sshql.Dialer
 var Db *sql.DB  //  プログラム中では一貫してこの変数を使うこと
 var Dberr error //  プログラム中では一貫してこの変数を使うこと
 
-func OpenDb(dbconfig *DBConfig) (err error) {
+var Tevent = "event"
+var Teventuser = "eventuser"
+var Tuser = "user"
+var Tuserhistory = "userhistory"
+
+
+func OpenDb(filenameofdbconfig string) (dbconfig *DBConfig, err error) {
 
 	//	https://leben.mobi/go/mysql-connect/practice/
 	//	OS := runtime.GOOS
@@ -62,35 +64,35 @@ func OpenDb(dbconfig *DBConfig) (err error) {
 	//	https://ssabcire.hatenablog.com/entry/2019/02/13/000722
 	//	https://konboi.hatenablog.com/entry/2016/04/12/100903
 
-	if dbconfig.Dbhost == "" {
-		dbconfig.Dbhost = "localhost"
+	dbconfig = new(DBConfig)
+	err = exsrapi.LoadConfig(filenameofdbconfig, &dbconfig)
+	if err != nil {
+		err = fmt.Errorf("exsrapi.Loadconfig(): %w", err)
+		return
 	}
-	if dbconfig.Dbport == "" {
-		dbconfig.Dbport = "3306"
+
+	if dbconfig.DBhost == "" {
+		dbconfig.DBhost = "localhost"
+	}
+	if dbconfig.DBport == "" {
+		dbconfig.DBport = "3306"
 	}
 	cnc := "@tcp"
 	if dbconfig.UseSSH {
-		err = exsrapi.LoadConfig("SSHConfig.yml", &sshconfig)
-		if err != nil {
-			err = fmt.Errorf("exsrapi.Loadconfig: %w", err)
-			return err
-		}
-		//	log.Printf("%+v\n", *sshconfig)
-
-		Dialer.Hostname = sshconfig.Hostname
-		Dialer.Port = sshconfig.Port
-		Dialer.Username = sshconfig.Username
-		Dialer.Password = sshconfig.Password
-		Dialer.PrivateKey = sshconfig.PrivateKey
+		Dialer.Hostname = dbconfig.SSHhost
+		Dialer.Port = dbconfig.SSHport
+		Dialer.Username = dbconfig.SSHuser
+		Dialer.Password = dbconfig.SSHpswd
+		Dialer.PrivateKey = dbconfig.SSHprvkey
 
 		mysqldrv.New(&Dialer).RegisterDial("ssh+tcp")
 		cnc = "@ssh+tcp"
 	}
-	cnc += "(" + dbconfig.Dbhost + ":" + dbconfig.Dbport + ")"
-	Db, Dberr = sql.Open("mysql", dbconfig.Dbuser+":"+dbconfig.Dbpw+cnc+"/"+dbconfig.Dbname+"?parseTime=true&loc=Asia%2FTokyo")
+	cnc += "(" + dbconfig.DBhost + ":" + dbconfig.DBport + ")"
+	Db, Dberr = sql.Open("mysql", dbconfig.DBuser+":"+dbconfig.DBpswd+cnc+"/"+dbconfig.DBname+"?parseTime=true&loc=Asia%2FTokyo")
 	if Dberr != nil {
 		err = fmt.Errorf("sql.Open(): %w", Dberr)
 	}
 
-	return err
+	return
 }
